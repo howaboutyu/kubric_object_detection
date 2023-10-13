02e6ee04e3fa20eddff451cf4b09683497e56f9c
higgsboost <higgsboost@gmail.com>
2023-10-13 22:05:25 +1300

    moved scripts into Makefile

--- start_data_generation.sh
+++
@@ -1,7 +0,0 @@
-docker run -it \
-	-v `pwd`:/kubric \
-	kubricdockerhub/kubruntu python /kubric/syndata_generator/kubric_generator.py \
-	--texture-dir syndata_generator/generated_textures  \
-	--output-path kubric_synthetic_data_output \
-	--num-generation 2000
-
--- start_tfrecord_generation.sh
+++
@@ -1,6 +0,0 @@
-docker run -it \
-	-v `pwd`:/kubric \
-	kubricdockerhub/kubruntu python /kubric/syndata_generator/tfrecord_creator.py \
-	--input-dir syndata_generator/random_texture \
-	--output-folder tfrecord_output_test
-
